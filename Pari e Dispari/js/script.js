6cd0ff8cc3a3891413f0885042dcede075810634
Steopa2001 <moraru495@gmail.com>
2025-07-07 15:57:49 +0200

    feat (es 2): user enters number from 1 to 5

--- Pari e Dispari/js/script.js
+++ Pari e Dispari/js/script.js
@@ -3,3 +3,5 @@
 const userChoice = prompt(`Scegli: pari o dispari`).toLowerCase();
 console.log(userChoice)
 
+const userNumber = parseInt(prompt(`Inserisci un numero da 1 a 5`));
+console.log(userNumber);
