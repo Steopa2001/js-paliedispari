e820bb939cd50c4e8cee217f0342e0c3ed6a95da
Steopa2001 <moraru495@gmail.com>
2025-07-07 15:35:15 +0200

    feat (es 1): end of function

--- Palidroma/js/script.js
+++ Palidroma/js/script.js
@@ -4,6 +4,14 @@ const userWord = prompt(`Inserisci una parola:`);
 
 //funzione per verificare se la parola è palindroma
 function isPalindroma(userInput) {
-  
+
+  //converto la parola tutta in minuscolo per evitare problemi di maiuscole
+  userInput = userInput.toLowerCase();
+
+  //inverto la parola 
+  const userInputInverted = userInput.split("").reverse().join("");
+
+  // verifico se la parola è uguale alla sua versione invertita
+  return userInput === userInputInverted;
 }
 
