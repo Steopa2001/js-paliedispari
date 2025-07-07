c4568227e8dd12231fe11c8f41d72f3257c0f14c
Steopa2001 <moraru495@gmail.com>
2025-07-07 16:01:56 +0200

    feat (es 2): generate random number with function

--- Pari e Dispari/js/script.js
+++ Pari e Dispari/js/script.js
@@ -5,3 +5,9 @@ console.log(userChoice)
 
 const userNumber = parseInt(prompt(`Inserisci un numero da 1 a 5`));
 console.log(userNumber);
+
+//funzione per generare un numero random da 1 a 5
+function randomNumber() {
+  return Math.floor(Math.random() * 5) + 1;
+}
+
