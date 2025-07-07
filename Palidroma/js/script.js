892bc0b88695db35bce3697adb847a93ccb4005c
Steopa2001 <moraru495@gmail.com>
2025-07-07 15:29:45 +0200

    feat (es 1): start function

--- Palidroma/js/script.js
+++ Palidroma/js/script.js
@@ -2,5 +2,8 @@
 // chiediamo all'utente di inserire una parola
 const userWord = prompt(`Inserisci una parola:`);
 
-
+//funzione per verificare se la parola è palindroma
+function isPalindroma(userInput) {
+  
+}
 
